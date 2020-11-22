a438257c898d51997eff13afefdb25b1fc80cc76
minhhieuhd98 <nguyenminhhieuhd98@gmail.com>
2020-11-22 21:27:03 +0700

    add field brand for products table

--- database/migrations/2020_11_09_081943_create_products_table.php
+++ database/migrations/2020_11_09_081943_create_products_table.php
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('brand');
             $table->string('link');
             $table->float('star');
             $table->text('description');
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -36,6 +36,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "Womens Olive Cape Sleeve Dress",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Tạo vẻ ngoài thanh lịch bằng cách mặc bộ sưu tập trang phục phương Tây độc đáo này từ nhà Zink London. Được tạo ra để làm nổi bật bất kỳ kiểu cơ thể nào, nó sẽ mang lại cho bạn sự quyến rũ hơn và khiến bạn nổi bật ở bất cứ đâu khi mặc chiếc áo này. Chất liệu sản phẩm cũng chắc chắn giúp bạn luôn thoải mái suốt cả ngày. Kết hợp nó với những phụ kiện thời trang tối thiểu, và tất nhiên, đừng quên nở nụ cười xinh xắn của bạn!",
                 'price' => $faker->numberBetween(1,100),
@@ -46,6 +47,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "Womens Regular Fit Off Shoulder Embroidered Midi Dress",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Tạo vẻ ngoài thanh lịch bằng cách mặc bộ sưu tập trang phục phương Tây độc đáo này từ nhà Zink London. Được tạo ra để làm nổi bật bất kỳ kiểu cơ thể nào, nó sẽ mang lại cho bạn sự quyến rũ hơn và khiến bạn nổi bật ở bất cứ đâu khi mặc chiếc áo này. Chất liệu sản phẩm cũng chắc chắn giúp bạn luôn thoải mái suốt cả ngày. Kết hợp nó với những phụ kiện thời trang tối thiểu, và tất nhiên, đừng quên nở nụ cười xinh xắn của bạn!",
                 'price' => $faker->numberBetween(1,100),
@@ -56,6 +58,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "Womens Ruffle Embellished Maxi",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Tạo vẻ ngoài thanh lịch bằng cách mặc bộ sưu tập trang phục phương Tây độc đáo này từ nhà Zink London. Được tạo ra để làm nổi bật bất kỳ kiểu cơ thể nào, nó sẽ mang lại cho bạn sự quyến rũ hơn và khiến bạn nổi bật ở bất cứ đâu khi mặc chiếc áo này. Chất liệu sản phẩm cũng chắc chắn giúp bạn luôn thoải mái suốt cả ngày. Kết hợp nó với những phụ kiện thời trang tối thiểu, và tất nhiên, đừng quên nở nụ cười xinh xắn của bạn!",
                 'price' => $faker->numberBetween(1,100),
@@ -66,6 +69,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "Womens Assymetrical Shift Dress",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Tạo vẻ ngoài thanh lịch bằng cách mặc bộ sưu tập trang phục phương Tây độc đáo này từ nhà Zink London. Được tạo ra để làm nổi bật bất kỳ kiểu cơ thể nào, nó sẽ mang lại cho bạn sự quyến rũ hơn và khiến bạn nổi bật ở bất cứ đâu khi mặc chiếc áo này. Chất liệu sản phẩm cũng chắc chắn giúp bạn luôn thoải mái suốt cả ngày. Kết hợp nó với những phụ kiện thời trang tối thiểu, và tất nhiên, đừng quên nở nụ cười xinh xắn của bạn!",
                 'price' => $faker->numberBetween(1,100),
@@ -76,6 +80,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "Womens Embroidered Shift Dress",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Tạo vẻ ngoài thanh lịch bằng cách mặc bộ sưu tập trang phục phương Tây độc đáo này từ nhà Zink London. Được tạo ra để làm nổi bật bất kỳ kiểu cơ thể nào, nó sẽ mang lại cho bạn sự quyến rũ hơn và khiến bạn nổi bật ở bất cứ đâu khi mặc chiếc áo này. Chất liệu sản phẩm cũng chắc chắn giúp bạn luôn thoải mái suốt cả ngày. Kết hợp nó với những phụ kiện thời trang tối thiểu, và tất nhiên, đừng quên nở nụ cười xinh xắn của bạn!",
                 'price' => $faker->numberBetween(1,100),
@@ -86,6 +91,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "Womens Regular Fit Boat Neck Printed Midi Dress",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Tạo vẻ ngoài thanh lịch bằng cách mặc bộ sưu tập trang phục phương Tây độc đáo này từ nhà Zink London. Được tạo ra để làm nổi bật bất kỳ kiểu cơ thể nào, nó sẽ mang lại cho bạn sự quyến rũ hơn và khiến bạn nổi bật ở bất cứ đâu khi mặc chiếc áo này. Chất liệu sản phẩm cũng chắc chắn giúp bạn luôn thoải mái suốt cả ngày. Kết hợp nó với những phụ kiện thời trang tối thiểu, và tất nhiên, đừng quên nở nụ cười xinh xắn của bạn!",
                 'price' => $faker->numberBetween(1,100),
@@ -96,6 +102,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "Womens Floral Embroidered Maxi",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Tạo vẻ ngoài thanh lịch bằng cách mặc bộ sưu tập trang phục phương Tây độc đáo này từ nhà Zink London. Được tạo ra để làm nổi bật bất kỳ kiểu cơ thể nào, nó sẽ mang lại cho bạn sự quyến rũ hơn và khiến bạn nổi bật ở bất cứ đâu khi mặc chiếc áo này. Chất liệu sản phẩm cũng chắc chắn giúp bạn luôn thoải mái suốt cả ngày. Kết hợp nó với những phụ kiện thời trang tối thiểu, và tất nhiên, đừng quên nở nụ cười xinh xắn của bạn!",
                 'price' => $faker->numberBetween(1,100),
@@ -106,6 +113,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "Womens Floral Gathered Maxi",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Tạo vẻ ngoài thanh lịch bằng cách mặc bộ sưu tập trang phục phương Tây độc đáo này từ nhà Zink London. Được tạo ra để làm nổi bật bất kỳ kiểu cơ thể nào, nó sẽ mang lại cho bạn sự quyến rũ hơn và khiến bạn nổi bật ở bất cứ đâu khi mặc chiếc áo này. Chất liệu sản phẩm cũng chắc chắn giúp bạn luôn thoải mái suốt cả ngày. Kết hợp nó với những phụ kiện thời trang tối thiểu, và tất nhiên, đừng quên nở nụ cười xinh xắn của bạn!",
                 'price' => $faker->numberBetween(1,100),
@@ -116,6 +124,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "Womens Blue Flowy Maxi",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Tạo vẻ ngoài thanh lịch bằng cách mặc bộ sưu tập trang phục phương Tây độc đáo này từ nhà Zink London. Được tạo ra để làm nổi bật bất kỳ kiểu cơ thể nào, nó sẽ mang lại cho bạn sự quyến rũ hơn và khiến bạn nổi bật ở bất cứ đâu khi mặc chiếc áo này. Chất liệu sản phẩm cũng chắc chắn giúp bạn luôn thoải mái suốt cả ngày. Kết hợp nó với những phụ kiện thời trang tối thiểu, và tất nhiên, đừng quên nở nụ cười xinh xắn của bạn!",
                 'price' => $faker->numberBetween(1,100),
@@ -126,6 +135,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "Womens Shimmer Cowl Dress",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Tạo vẻ ngoài thanh lịch bằng cách mặc bộ sưu tập trang phục phương Tây độc đáo này từ nhà Zink London. Được tạo ra để làm nổi bật bất kỳ kiểu cơ thể nào, nó sẽ mang lại cho bạn sự quyến rũ hơn và khiến bạn nổi bật ở bất cứ đâu khi mặc chiếc áo này. Chất liệu sản phẩm cũng chắc chắn giúp bạn luôn thoải mái suốt cả ngày. Kết hợp nó với những phụ kiện thời trang tối thiểu, và tất nhiên, đừng quên nở nụ cười xinh xắn của bạn!",
                 'price' => $faker->numberBetween(1,100),
@@ -141,6 +151,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "PUMA",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Hãy tập thể dục thật phong cách bằng cách mang đôi giày thể thao này của Reebok và nhận được những lời khen ngợi cho sự lựa chọn của bạn. Được làm từ chất liệu chất lượng tốt, đảm bảo độ ôm tay tốt hơn, đồng thời ren lên cố định tạo sự thuận tiện khi mặc.",
                 'price' => $faker->numberBetween(1,100),
@@ -151,6 +162,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "CATWALK",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Sàn catwalk của Gothic. Trở Lại Thời Trang Với Những Đôi Bốt Có Gót Thường. Xu hướng đế Chunky, đế đệm trong đế khối PU, biến ngón chân rắn bằng sáng chế này trở thành một lựa chọn thích hợp! 100% Sản xuất tại Ấn Độ.",
                 'price' => $faker->numberBetween(1,100),
@@ -161,6 +173,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "REEBOK",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Gây ấn tượng với mọi người bằng sự lựa chọn giày dép của bạn bằng cách mang đôi giày thể thao này của Reebok. Nó đã được làm bằng vật liệu chất lượng tốt sẽ cung cấp cho bạn một kết thúc sang trọng. Hơn nữa, nó có kiểu dáng slip on tạo sự thuận tiện khi đeo.",
                 'price' => $faker->numberBetween(1,100),
@@ -171,6 +184,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "Mens Grey Athleisure Sports Range Walking Shoes",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Một sự pha trộn hoàn hảo giữa phong cách và sự thoải mái, đôi giày thể thao màu xám này của Red Tape là sản phẩm cần có cho những người đàn ông hiện đại. Có thiết kế kết cấu hợp thời trang và đế sành điệu, những chiếc áo ren này lý tưởng cho các buổi chạy bộ, đi bộ hoặc tập gym. Với phần trên bằng vải dệt, những đôi giày thể thao này bền, nhẹ và dễ bảo trì. Đế EVA (ethylene vinyl acetate) mang lại sự thoải mái nhẹ.",
                 'price' => $faker->numberBetween(1,100),
@@ -181,6 +195,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "ADIDAS",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Đế giữa IMEVA ánh sáng với lớp adiprene có thể nhìn thấy giúp bảo vệ khỏi các lực tác động có hại. Tiếp thêm sức mạnh cho buổi chạy của bạn với đôi giày thể thao này của Adidas, được thiết kế cho những người muốn tối đa hóa năng lượng và chuyển động trong suốt quá trình tập luyện cường độ cao. Nó có tính năng buộc ren lên, giúp bạn dễ mặc. Hơn nữa, nó đã được làm bằng chất liệu chất lượng tốt mang lại cảm giác cầm nắm chắc chắn.",
                 'price' => $faker->numberBetween(1,100),
@@ -191,6 +206,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "SKECHERS",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Hãy làm cho vẻ ngoài của bạn trở nên đáng chú ý bằng cách mang đôi giày thể thao này của Skechers. Nó đã được làm bằng chất liệu chất lượng tốt, mang lại cho nó một lớp hoàn thiện tốt trong khi ren buộc lên tạo sự thuận tiện khi mặc. Bên cạnh đó, đôi giày thể thao này có độ bền cao, trọng lượng nhẹ và mang lại cảm giác thoải mái khi mang.",
                 'price' => $faker->numberBetween(1,100),
@@ -201,6 +217,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "GUESS",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Ghi điểm cao trên bảng đồng hồ thời trang bằng cách đi đôi bốt này do thương hiệu nổi tiếng Guess giới thiệu cho bạn. Đặc trưng với phần mũi giày nhọn, đồng thời có khóa kéo tạo sự thuận tiện khi mang. Hơn nữa, nó có đế ngoài bằng cao su nhiệt dẻo sẽ cung cấp cho bạn lực kéo tối ưu.",
                 'price' => $faker->numberBetween(1,100),
@@ -211,6 +228,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "TRESMODE",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Bước vào một thế giới thoải mái với giày thể thao xỏ ngón The Noa Grey dành cho phụ nữ. Được làm từ chất liệu vải tổng hợp cao cấp, phần trên mềm mại và lý tưởng cho mùa gió chướng vì tính năng nhanh khô. Đế TPR chunky làm cho đôi này chắc chắn và lý tưởng cho những chuyến đi bộ dài. Mẹo phong cách- Kết hợp đôi giày thể thao này với một chiếc váy dài đến đầu gối để có vẻ ngoài dễ thương.",
                 'price' => $faker->numberBetween(1,100),
@@ -221,6 +239,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "VS PACE Men Lace Up Sneakers",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Phong cách dễ dàng, ổn định với mọi bước đi. Đôi giày lấy cảm hứng từ bóng rổ này có phần da giống như trên với đường khâu - Trên 3 đường sọc ở một bên và đục lỗ 3 sọc ở mặt khác. Đế ngoài bằng cao su mang đến cho chúng một cái nhìn hoàn thiện. Thêm sức hút vào bộ sưu tập giày dép của bạn với đôi giày thể thao này của Adidas. Nó có một dây buộc lên giúp bạn mặc nó một cách dễ dàng. Bên cạnh đó, phần trên được chế tác từ da tổng hợp chất lượng cao mang lại độ hoàn thiện tốt, trong khi phần đế ngoài được làm bằng ethylene vinyl axetat mang lại cảm giác cầm nắm tốt hơn.",
                 'price' => $faker->numberBetween(1,100),
@@ -231,6 +250,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => "Mens Leather Lace Up Oxfords",
                 'link' => $faker->url,
+                'brand' => 'No brand',
                 'star' => $faker->randomFloat(null, 0, 5),
                 'description' => "Nâng cao bộ sưu tập giày dép của bạn bằng cách thêm đôi oxfords này từ nhà Lee Cooper. Nó được làm thủ công từ da chất lượng tốt cho độ hoàn thiện tốt, trong khi đế ngoài bằng cao su nhiệt dẻo giúp chống trơn trượt. Trên hết, nó có một dây buộc để đảm bảo bạn vừa khít.",
                 'price' => $faker->numberBetween(1,100),
