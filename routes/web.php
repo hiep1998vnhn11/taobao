ab9e4e946c75b83c76fa652f70b3d85266a7a283
hoang cong thanh <hoangcongthanh1237d@gmail.com>
2020-11-22 15:11:15 +0700

    fix middleware auth + taobao logo in all pages

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\ProductHomeController::class,'show'])->name('/');
 Auth::routes();
 
-Route::get('/cart',[OrderController::class, 'index'])->name('order_by_user');
-Route::post('/addToCart',[OrderController::class, 'store'])->name('add_to_cart');
-Route::delete('/deleteItem', [OrderController::class, 'destroy'])->name('del_item');
-Route::post('/updateItem', [OrderController::class, 'update'])->name('update_item');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/cart', [OrderController::class, 'index'])->name('order_by_user');
+    Route::get('/checkout', function () {
+        return view('checkout');
+    });
+    Route::post('/addToCart',[OrderController::class, 'store'])->name('add_to_cart');
+    Route::delete('/deleteItem', [OrderController::class, 'destroy'])->name('del_item');
+    Route::post('/updateItem', [OrderController::class, 'update'])->name('update_item');
+});
 
 Route::get('/product-detail/{id}', 'ProductController@getProduct')->name('product-detail');
 
 Route::get('products/{category_id}',[ProductCategoryController::class, 'show']);
 
-Route::get('/checkout', function(){
-    return view('checkout');
-});
+
 
